804930ab616069851fa3baf6f5a63675fca7c355
“João <eng.joao.pedro@outlook.com>
2022-12-28 18:06:13 -0300

    refactor: functionality to show text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setText(data);
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > data.length) {
+      amount = data.length;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
